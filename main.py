d67fa7d3fe0a5f98b7c06461baca87125ff05c84
Jaehoonoo <justjhoon@gmail.com>
2024-09-14 18:30:59 -0400

    .

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ from taipy.gui import Markdown
 import pandas as pd
 
 
-# cmd + shift + G to open usr/local/bin and find python interpreter path (Mac)
-
-
 # Runs the pages on a local port
 # Links external css file
 Gui(page='Starter', css_file='main.css').run(use_reloader='true', port=5001)
